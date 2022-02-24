ff63c0faba756ac0002a0c22cf71a95d8c9bdc5f
rumgalxdead <rumgal@gmail.com>
2022-02-24 17:28:16 +0100

    test ok

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,16 @@
 			<span>Nombre total de paires : 9</span> | <span>Nombre de tentatives : {{ nbTentative }} </span> |
 			<span>Temps de jeu : {{ timestamp }} </span> |
 			<span>Nombre de paires trouvées : {{ pairesTrouvées }} </span>
-			<button disabled class="btn btn-danger" id="recommencer">Recommencer</button>
+			<button disabled class="btn btn-danger" id="recommencer" @click.self="recommencer">Recommencer</button>
 		</div>
 		<div class="cards">
-			<div class="fireworks" style="display: none"></div>
+			<div class="fireworks" style="display: none">
+				<h1 class="top">BRAVOOOOOOOO</h1>
+			</div>
 			<div class="card" v-for="(el, i) in pairesImages" :key="i" :data-id="el.id" @click.self="rotate">
-				<!-- <div class="card rotate" v-for="i in images" :key="i" data-id="el.id"> -->
 				<div class="card__front">
 					<div class="card-body">
 						<img :src="el.img" alt="un animal" class="rounded" />
-						<!-- <img :src="i" alt="un animal" class="rounded" /> -->
 					</div>
 				</div>
 			</div>
@@ -39,8 +39,8 @@ export default {
 			],
 			pairesImages: [],
 			nbTentative: 0,
-			pairesTrouvées: 0,
 			paires: [],
+			pairesTrouvées: 0,
 			timestamp: 0,
 			timer: null,
 			startTimer: false,
@@ -67,6 +67,7 @@ export default {
 		},
 	},
 	methods: {
+		// le melange des cartes
 		melange() {
 			let createPairs = [];
 			for (let i = 0; i < this.images.length; i++) {
@@ -74,59 +75,65 @@ export default {
 			}
 			this.pairesImages = createPairs.sort(() => 0.5 - Math.random());
 		},
-	},
-
-	rotate(e) {
-		this.startTimer = true;
-		document.querySelector("#recommencer").removeAttribute("disabled");
-		// retourner la carte
-		//  retourner une autre carte
-		// verifier si elles sont paires
-		// si paires  ( inc nb paire trouver + tentative + timerstart)
-		// sinon retourner les deux cartes ( inc tentative + timerstart)
-		if (this.paires.length < 2) {
-			e.target.classList.toggle("rotate");
-			console.log("je retourne la carte");
-			if (this.paires.length === 2) {
-				this.nbTentative++;
-				if (this.paires[0].dataset.id === this.paires[1].dataset.id) {
-					this.pairesTrouvées++;
-					this.paires = [];
-				} else {
-					setTimeout(() => {
-						this.paires[0].classList.remove("rotate");
-						this.paires[1].classList.remove("rotate");
+		// la rotation des cartes
+		rotate(e) {
+			this.startTimer = true;
+			document.querySelector("#recommencer").removeAttribute("disabled");
+			// retourner la carte
+			//  retourner une autre carte
+			// verifier si elles sont paires
+			// si paires  ( inc nb paire trouver + tentative + timerstart)
+			// sinon retourner les deux cartes ( inc tentative + timerstart)
+			if (this.paires.length < 2) {
+				e.target.classList.toggle("rotate");
+				this.paires.push(e.target);
+				console.log("je retourne la carte");
+				if (this.paires.length === 2) {
+					this.nbTentative++;
+					if (this.paires[0].dataset.id === this.paires[1].dataset.id) {
+						this.pairesTrouvées++;
 						this.paires = [];
-					}, 1000);
+					} else {
+						setTimeout(() => {
+							this.paires[0].classList.remove("rotate");
+							this.paires[1].classList.remove("rotate");
+							this.paires = [];
+						}, 1000);
+					}
 				}
 			}
-		}
-		if (this.pairesTrouvées === 9) {
+			if (this.pairesTrouvées === 9) {
+				clearInterval(this.timer);
+				document.querySelector("#recommencer").innerHTML = "Rejouer";
+			}
+			if (this.pairesTrouvées === 1) {
+				document.querySelector(".fireworks").style.display = "block";
+			}
+		},
+		// restart
+		recommencer(e) {
+			this.nbTentative = 0;
+			this.nbTrouvées = 0;
+			this.timestamp = 0;
+			e.target.setAttribute("disabled", "");
 			clearInterval(this.timer);
-			document.querySelector("#recommencer").innerHTML = "Rejouer";
-		}
+			this.reset();
+			this.melange();
+		},
+		// init
+		reset() {
+			Array.from(document.querySelectorAll(".card")).forEach((card) => {
+				card.classList.remove("rotate");
+			});
+		},
 	},
-	recommencer(e) {
-		this.nbTentative = 0;
-		this.nbTrouvées = 0;
-		this.timestamp = 0;
-		e.target.setAttribute("disabled");
-		clearInterval(this.timer);
-		this.reset();
+	mounted() {
 		this.melange();
 	},
-	reset() {
-		Array.from(document.querySelectorAll(".card")).forEach((card) => {
-			card.classList.remove("rotate");
-		});
-	},
-
-	mounted() {
-		this.melange;
-	},
-
 	components: {},
 };
 </script>
 
 <style lang="scss" src="./assets/app.scss"></style>
+
+// perfectionner
